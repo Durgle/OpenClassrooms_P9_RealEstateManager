e4139b5be7f02b04734e2dfc55b253698c22161a
Durgle <durgle02@gmail.com>
2024-06-21 01:54:42 +0200

    remove unnecessary code

--- app/src/main/java/com/openclassrooms/realestatemanager/worker/WorkManager.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/worker/WorkManager.kt
@@ -4,17 +4,11 @@ import android.content.Context
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 
-class WorkManager(appContext: Context) {
-
-    val context: Context
-
-    init {
-        context = appContext
-    }
+class WorkManager(private val appContext: Context) {
 
     fun scheduleEstateNotification() {
         val workRequest = OneTimeWorkRequest.Builder(EstateNotificationWorker::class.java)
             .build()
-        WorkManager.getInstance(context).enqueue(workRequest)
+        WorkManager.getInstance(appContext).enqueue(workRequest)
     }
 }
